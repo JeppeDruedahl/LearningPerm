cbdf6322a0a768c3d858bd23098665eaadb7a384
Thomas Høgholm Jørgensen <bnl429@ku.dk>
2020-01-06 11:48:13 +0100

    time everything

--- run_00_all.m
+++ run_00_all.m
@@ -8,6 +8,10 @@ compile_mex('mex_simulate',threads,0,intel);
 compile_mex('mex_calc_logdiffs',threads,0,intel);
 compile_mex('mex_calc_covs',threads,0,intel);
 
+% time at start
+file = fopen('timings.txt','w');
+fprintf(file,'started at %s \n',datestr(datetime('now')));
+
 %% scripts
 
 run_01_data
@@ -16,19 +20,14 @@ run_03_estimate
 run_04_estimate_prefs
 run_05_robustness
 
-% Monte Carlo: choose baseline or small 
-% Baseline reproduces results in the paper. _small is much faster (5 MC runs vs. 200) but does not reproduce results in the paper.
-% We write the start and end timings to calculate the expected computation time
-file = fopen('timings.txt','w');
-fprintf(file,'started at %s \n',datestr(datetime('now')));
-
+% Monte Carlo: choose baseline or small. _small is much faster (5 MC runs vs. 200) but does not reproduce results in the paper.
 run_06_monte_carlo
 %run_06_monte_carlo_small
 
+run_07_tables
+run_08_Commault
+
+% time at end
 file = fopen('timings.txt','w');
 fprintf(file,'ended at %s \n',datestr(datetime('now')));
 fclose(file);
-
-% Tables
-run_07_tables
-run_08_Commault
