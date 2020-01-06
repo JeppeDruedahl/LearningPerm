30f8065c4b0cc0337a1e07c3d70a9ef0e08489c4
Thomas Høgholm Jørgensen <bnl429@ku.dk>
2020-01-06 11:34:04 +0100

    timing in run_00_all + run_06_monte_carlo_small

--- run_00_all.m
+++ run_00_all.m
@@ -15,6 +15,20 @@ run_02_ceq
 run_03_estimate
 run_04_estimate_prefs
 run_05_robustness
+
+% Monte Carlo: choose baseline or small 
+% Baseline reproduces results in the paper. _small is much faster (5 MC runs vs. 200) but does not reproduce results in the paper.
+% We write the start and end timings to calculate the expected computation time
+file = fopen('timings.txt','w');
+fprintf(file,'started at %s \n',datestr(datetime('now')));
+
 run_06_monte_carlo
+%run_06_monte_carlo_small
+
+file = fopen('timings.txt','w');
+fprintf(file,'ended at %s \n',datestr(datetime('now')));
+fclose(file);
+
+% Tables
 run_07_tables
 run_08_Commault
