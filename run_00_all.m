9b8b0ee95e4152af04f5d05805d6cb76aef15784
ThomasHJorgensen <tjo@econ.ku.dk>
2020-01-20 14:41:39 +0100

    Update run_00_all wrt timing and small MC

--- run_00_all.m
+++ run_00_all.m
@@ -21,13 +21,16 @@ run_04_estimate_prefs
 run_05_robustness
 
 % Monte Carlo: choose baseline or small. _small is much faster (5 MC runs vs. 200) but does not reproduce results in the paper.
-run_06_monte_carlo
-%run_06_monte_carlo_small
+DO_SMALL = 0;
+if DO_SMALL == 0
+  run_06_monte_carlo
+else
+  run_06_monte_carlo_small
+end
 
 run_07_tables
 run_08_Commault
 
 % time at end
-file = fopen('timings.txt','w');
 fprintf(file,'ended at %s \n',datestr(datetime('now')));
 fclose(file);
